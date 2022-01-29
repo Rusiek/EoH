59864dd2076265988409164e14854c1f9249012b
Rusiek <szymon.rusiecki@protonmail.com>
2022-01-29 17:02:29 +0100

    Basic clock feature

---
+++ src/clock.h
@@ -0,0 +1,27 @@
+#ifndef CLOCK_H
+#define CLOCK_H
+#include "human.h"
+
+struct Time{
+    int year;
+    int month;
+
+    Time();
+
+    void next_month();
+};
+
+Time::Time() {
+    year = 0;
+    month = 0;
+}
+
+void Time::next_month() {
+    ++month;
+    if (month == 12)
+        month = 0, ++year;
+}
+
+
+
+#endif
