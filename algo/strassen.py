082c930d4567d1966a31d00f5da3713f22ad5fd2
Rusiek <szymon.rusiecki@protonmail.com>
2022-02-20 16:46:35 +0100

    Update strassen.py

--- algo/strassen.py
+++ algo/strassen.py
@@ -29,16 +29,9 @@ def split(M):
     h_row = len(M[0]) // 2
     h_col = len(M) // 2
     
-    M11 = [[M[i][j] for j in range(h_row)] for i in range(h_col)]
-    M12 = [[M[i][j] for j in range(h_row, len(M[0]))] for i in range(h_col)]
-    M21 = [[M[i][j] for j in range(h_row)] for i in range(h_col, len(M))]
-    M22 = [[M[i][j] for j in range(h_row, len(M[0]))] for i in range(h_col, len(M))]
+    M11 = [[M[i][j] for j in range(h_row)]              for i in range(h_col)]
+    M12 = [[M[i][j] for j in range(h_row, len(M[0]))]   for i in range(h_col)]
+    M21 = [[M[i][j] for j in range(h_row)]              for i in range(h_col, len(M))]
+    M22 = [[M[i][j] for j in range(h_row, len(M[0]))]   for i in range(h_col, len(M))]
     
     return M11, M12, M21, M22
-
-
-# def strassen(A, B):
-
-for i in split([[1, 2, 3, 4, 6], [4, 5, 6, 7, 2], [1, 3, 5, 6, 1], [9, 8, 7, 6, 5], [1, 3, 5, 3, 2]]):
-    print(i)
-    
